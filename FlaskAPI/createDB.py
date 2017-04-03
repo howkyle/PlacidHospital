791866071d093e190ac814c46c8f48d4a20af5f5
alexleslie <alex.leslie.b@gmail.com>
2017-04-03 12:20:31 -0500

    foreign keys are in

--- FlaskAPI/createDB.py
+++ FlaskAPI/createDB.py
@@ -17,7 +17,7 @@ sql  = "INSERT INTO doctor(first_name, last_name, category, specializatoin, pass
 connection.execute(sql)
 
 #Considering adding a foreign key for doctor ID in patient table
-connection.execute("CREATE TABLE patient(id SERIAL PRIMARY KEY, first_name varchar(50),last_name varchar(50), patient_number varchar(50));")
+connection.execute("CREATE TABLE patient(id SERIAL PRIMARY KEY, first_name varchar(50),last_name varchar(50), patient_number varchar(50), doctor SERIAL references doctor(id));")
 sql = "INSERT INTO patient(first_name, last_name, patient_number) VALUES ('John', 'Larrington', '444-2424');"
 connection.execute(sql)
 
@@ -38,32 +38,27 @@ sql = "INSERT INTO disease(disease_name, type) VALUES ('Cholera','Water-borne');
 connection.execute(sql)
 
 #Need foreign key for disease and patient id
-connection.execute("CREATE TABLE family_history(id SERIAL PRIMARY KEY, first_name varchar(50), last_name varchar(50));")
+connection.execute("CREATE TABLE family_history(id SERIAL PRIMARY KEY, first_name varchar(50), last_name varchar(50), disease int references disease(id), patient int references patient(id));")
 sql = "INSERT INTO family_history(first_name, last_name) VALUES ('Kik', 'Anthony');"
 connection.execute(sql)
 
 #Need foreign key for patient id and doctor id
-connection.execute("CREATE TABLE procedure(id SERIAL PRIMARY KEY, test varchar(50));")
+connection.execute("CREATE TABLE procedure(id SERIAL PRIMARY KEY, test varchar(50), patient int references patient(id), doctor int references doctor(id));")
 sql = "INSERT INTO procedure(test) VALUES ('blood test');"
 connection.execute(sql)
 
 #Need foreign key for procedure id
-connection.execute("CREATE TABLE test_results(id SERIAL PRIMARY KEY, symptoms varchar(50));")
+connection.execute("CREATE TABLE test_results(id SERIAL PRIMARY KEY, symptoms varchar(50), procedure int references procedure(id));")
 sql = "INSERT INTO test_results(symptoms) VALUES ('vomiting');"
 connection.execute(sql)
 
-#Need foreign key for test_results id
-connection.execute("CREATE TABLE scans(id SERIAL PRIMARY KEY, scanImg varchar(50));")
-sql = "INSERT INTO scans(scanImg) VALUES ('image');"
-connection.execute(sql)
-
 #Need foreign key for disease id and test_results id
-connection.execute("CREATE TABLE diagnosis(id SERIAL PRIMARY KEY, cause_of_disease varchar(50), diagnosis_date date);")
+connection.execute("CREATE TABLE diagnosis(id SERIAL PRIMARY KEY, diagnosis_date date, disease int references disease(id), results int references test_results(id));")
 sql = "INSERT INTO diagnosis(cause_of_disease, diagnosis_date) VALUES ('contaminated water', '2017-03-20');"
 connection.execute(sql)
 
 #Need foreign key for diagnosis id
-connection.execute("CREATE TABLE treatment(id SERIAL PRIMARY KEY, treatment_date date);")
+connection.execute("CREATE TABLE treatment(id SERIAL PRIMARY KEY, treatment_date date, diagnosis int references diagnosis(id));")
 sql = "INSERT INTO treatment(treatment_date) VALUES ('2017-03-25');"
 connection.execute(sql)
 
@@ -71,6 +66,6 @@ connection.execute("CREATE TABLE medication(id SERIAL PRIMARY KEY, name varchar(
 sql = "INSERT INTO medication(name, dosage) VALUES ('panadol', '20 tables');"
 connection.execute(sql)
 
-connection.execute("CREATE TABLE allergy(id SERIAL PRIMARY KEY, allergy_name varchar(50));")
+connection.execute("CREATE TABLE allergy(id SERIAL PRIMARY KEY, allergy_name varchar(50), medication int references medication(id));")
 sql = "INSERT INTO allergy(allergy_name) VALUES ('none');"
 connection.execute(sql)
