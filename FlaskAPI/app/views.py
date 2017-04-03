8369a4633256b0eaeca5780c15e9ea17c6addfa3
Kyle Howard <howardka@hotmail.com>
2017-04-03 16:15:12 -0500

    register patient route added

--- FlaskAPI/app/views.py
+++ FlaskAPI/app/views.py
@@ -2,7 +2,7 @@ from app import app,db, connection
 from flask import render_template, request, redirect, url_for, flash, jsonify
 # from forms import *
 # from models import*
-import json
+import json, datetime
 
 
 @app.route("/")
@@ -20,7 +20,7 @@ def doctorLogin():
 		if password == doc["password"]:
 			return "{'result':'login success','id':"+str(doc["id"])+"}"
 		else:
-			return "{'result':'invalid password','id':"+str(doc["id"])+"}"
+			return "{'result':'invalid password'}"
 
 @app.route("/nurse_login", methods=["POST"])
 def nurseLogin():
@@ -33,7 +33,7 @@ def nurseLogin():
 		if password == nurse["password"]:
 			return "{'result':'login success','id':"+str(nurse["id"])+"}"
 		else:
-			return "{'result':'invalid password','id':"+str(nurse["id"])+"}"
+			return "{'result':'invalid password'}"
 
 
 @app.route("/secretary_login", methods = ["POST"])
@@ -47,7 +47,27 @@ def secretaryLogin():
 		if password == sec["password"]:
 			return "{'result':'login success','id':"+str(sec["id"])+"}"
 		else:
-			return "{'result':'invalid password','id':"+str(sec["id"])+"}"
+			return "{'result':'invalid password'}"
+
+app.route("/register_patient", methods =["POST"])
+def registerPatient():
+	if request.method == "POST":
+		time_added = datetime.datetime.now()
+		patient = request.jsonify
+		sql = "insert into patient(first_name,last_name,address, dob, phone_number, date_added) values("+str(patient['first_name'])+","+str(patient['last_name'])+","+str(patient['address'])+","+str(patient['dob'])+","+str(patient['phone_number'])+","+str(patient['doctor_id'])+","+str(time_added)+");"
+		try:
+			connection.execute(sql)
+		except:
+			return "{'result':'unable to register patient'}"
+		else:
+			sql = "select * from patient where date_added ="+str(time_added)+";"
+			result = connection.execute(sql)
+			if result:
+				for row in result:
+					pid = row[0]
+					print pid
+
+				return {"first_name":str(patient['first_name']),"last_name":str(patient['last_name']),"address":str(patient['address']),"dob":str(patient['dob']),"phone_number":str(patient['phone_number']),"doctor_id":str(patient['doctor_id'])}
 
 
 
