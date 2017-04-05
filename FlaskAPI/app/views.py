d294e6a88b252f818ae3390e2892754c44c0fc8b
Kyle Howard <howardka@hotmail.com>
2017-04-05 16:14:35 -0500

    need to complete vital signs

--- FlaskAPI/app/views.py
+++ FlaskAPI/app/views.py
@@ -29,7 +29,7 @@ def doctorLogin():
 def nurseLogin():
 	if request.method =="POST":
 		nurse = request.json
-		result = connection.execute("Select password from nurse where id="+str(nurse["id"])+";")
+		result = connection.execute("Select password, first_name, last_name from nurse where id="+str(nurse["id"])+";")
 		for row in result:
 			password = row[0]
 			first_name  = row[1]
@@ -179,10 +179,97 @@ def getTestResults(pid):
 			return json.dumps(test_results, ensure_ascii=False)
 
 
-	@app.route("/get_medical_data/<pid>", methods = ["GET"])
-	def getMedicalData(pid):
-		if request.method == "GET":
-			sql = "select "
+@app.route("/get_medical_data/<pid>", methods = ["GET"])
+def getMedicalData(pid):
+	if request.method == "GET":
+		sql = "select test, results, conclusion, medication.name from (((((medication inner join treatment_medication on medication.id = treatment_medication.medication) inner join treatment on treatment_medication.treatment = treatment.id) inner join diagnosis on treatment.diagnosisid = diagnosis.id) inner join test_results on diagnosis.resultid = test_results.id) inner join procedure on test_results.procedure = procedure.id) where procedure.patient = "+str(pid)+";"
+		
+		try:
+			result = connection.execute(sql)
+			medDict = {}
+			med_data = []
+			count = 0
+			for row in result:
+				count = count+1
+				print count
+				medDict["procedure"] = row[0]
+				medDict["result"] = row[1]
+				medDict["diagnosis"] = row[2]
+				medDict["treatment"] = row[3]
+				med_data.append(medDict)
+		except:
+			return "{'status':'failure'}"
+		else:
+			print len(med_data)
+			return json.dumps(med_data, ensure_ascii=False)
+
+@app.route("/add_medical_data/<pid>", methods=["POST"])
+def addMedicalData(pid):
+	if request.method == "POST":
+		data = request.json 
+		procedure = data["procedure"]
+		test_result = data["result"]
+		diagnosis = data["diagnosis"]
+		treatment = data["treatment"]
+		doctorid = data["doctor_id"]
+
+		count = 0
+
+		try:
+			sql = "insert into procedure(test, patient) values(\'"+str(procedure)+"\',"+str(pid)+");"
+			connection.execute(sql)
+
+			sql = "select id from procedure order by id desc limit 1;"
+			result = connection.execute(sql)
+			for row in result:
+				procedure_id = row[0]
+
+			sql = "insert into test_results(results, procedure) values(\'"+str(test_result)+"\',"+str(procedure_id)+");"
+			connection.execute(sql)
+
+			sql = "select id from test_results order by id desc limit 1;"
+			result = connection.execute(sql)
+			for row in result:
+				result_id = row[0]
+
+			cur_date = '2005-5-4'
+			sql = "insert into diagnosis(conclusion, diagnosis_date, resultid) values(\'"+str(diagnosis)+"\',\'"+str(cur_date)+"\',"+str(result_id)+");"
+			connection.execute(sql)
+
+			sql = "select id from diagnosis order by id desc limit 1;"
+			result = connection.execute(sql)
+			for row in result:
+				diagnosis_id = row[0]
+
+			sql = "insert into treatment(doctorid, patientid,treatment_date, diagnosisid) values("+str(doctorid)+","+str(pid)+",\'"+str(cur_date)+"\',"+str(diagnosis_id)+");"
+			connection.execute(sql)
+			sql = "select id from treatment order by id desc limit 1;"
+			result = connection.execute(sql)
+			for row in result:
+				treatment_id = row[0]
+				print treatment_id
+				print treatment
+
+			sql = "select id from medication where name =\'"+str(treatment)+"\';"
+			result = connection.execute(sql)
+			for row in result:
+				med_id = row[0]
+
+		
+			sql = "insert into treatment_medication(medication, treatment) values("+str(med_id)+","+str(treatment_id)+");"
+			connection.execute(sql)
+
+			count= count + 1
+			print count
+
+		except:
+			return "{'status':'failure'}"
+		else:
+			return "{'status':'success'}"
+
+
+
+
 
 
 @app.route("/get_interns", methods =["GET"])
@@ -200,6 +287,43 @@ def getInterns():
 			return "{'status':'failure'}"
 		else:
 			return json.dumps(interns, ensure_ascii=False)
+
+
+@app.route("/get_vital_signs/<pid>", methods = ["GET"])
+def getVitalSigns(pid):
+	if request.method =="GET":
+		sql = "select sign from vital_signs where patientid ="+str(pid)+";"
+		try:
+			result = connection.execute(sql)
+			signs = []
+
+			for row in result:
+				signs.append(row[0])
+		except:
+			return "{'status':'failure'}"
+		else:
+			return json.dumps(signs, ensure_ascii=False)
+
+@app.route("/add_vital_sign/<pid>", methods =["POST"])
+def addVitalSign(pid):
+	if request.method == "POST":
+		pass
+
+		data = request.json
+		nurse_id = data["nurse_id"]
+		sign = data["vital"]
+
+		sql = "insert into vital_signs(nurseID, sign, patientid) values ("+str(nurse_id)+",\'"+str(sign)+"\',"+str(pid)+";"
+
+		try :
+			connection.execute(sql)
+		except:
+			return "{'status':'failure'}"
+		else:
+			return "{'status':'success'}"
+
+
+
 
 
 
